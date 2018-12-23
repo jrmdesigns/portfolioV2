a7bf06d5c874f751aebe364b5c8a2d2d1ff36b5b
= <=>
2018-12-23 18:06:19 +0100

    work and project

--- js/main.js
+++ js/main.js
@@ -4,14 +4,11 @@ var menuItems = [];
 var home, homeOverlay;
 var anchorPoints = [];
 var anchorLocation = [];
+var hideAbleElements = [];
 var anchorIndex = 0;
 var waiting = false;
-var canScroll = true;
+var canScroll = false;
 var offset = 0;
-var horizontalSlider = [];
-var horizontalSliderIndex = 0;
-var horizontalSliderIndexTemp = 0;
-var horizontalSliderRightButton;
 window.onload = function(){
   menu = document.getElementById("menu");
   menuButton = document.getElementById("menubutton");
@@ -19,8 +16,11 @@ window.onload = function(){
   menuItems = document.getElementsByClassName("js-menu-item");
   home = document.getElementById("home");
   homeOverlay = document.getElementById("home-overlay");
-  console.log(home.tagName);
-  console.log(menuButton);
+  hideAbleElements = $(".js-hide");
+
+  // console.log(home.tagName);
+  // console.log(menuButton);
+  console.log(hideAbleElements);
   
   if(menuLinks.length != menuItems.length){
     document.write("ERROR FIX MENU");
@@ -41,13 +41,9 @@ window.onload = function(){
     }
 
     window.scrollTo(0, 0);
-    console.log(anchorLocation);
+    // console.log(anchorLocation);
 
-  
-  
-  horizontalSlider = document.getElementsByClassName("js-horizontal-slider");
-  horizontalSliderRightButton = document.getElementsByClassName("js-horizontal-button");
-  horizontalSlider[horizontalSliderIndex].style.display = "block";
+    canScroll = true; 
 }
 
 
@@ -61,48 +57,9 @@ function resizeWindow(){
     }
     window.scrollTo(0,0);
     anchorIndex = 0;
+
+
 }
-
-
-$(document).on('mousewheel DOMMouseScroll', function (e) {
-    if(waiting || canScroll == false){
-      return;
-    }
-    e.preventDefault();//prevent the default mousewheel scrolling
-    var active = $('section.active');
-    var delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1;
-    waiting = true;
-    if (delta < 0) {
-      anchorIndex += 1;
-      if(anchorIndex > (anchorPoints.length -1)){
-        anchorIndex = 0;
-      }
-      scrollTo({
-        top: anchorLocation[anchorIndex],
-        left: 0,
-        behavior: 'smooth'
-      });
-
-      console.log(anchorIndex);
-      console.log('scrolling down');
-    } else {
-      anchorIndex -= 1;
-      if(anchorIndex < 0){
-        anchorIndex = anchorPoints.length - 1;
-
-      }
-      scrollTo({
-        top: anchorLocation[anchorIndex],
-        left: 0,
-        behavior: 'smooth'
-      });
-      console.log(anchorIndex);
-      console.log('scrolling up');
-    }
-setTimeout(function(){
-    waiting = false;
-  }, 1000);
-  });
 
 function showMenu(index){
   return function(){
@@ -116,7 +73,7 @@ function showMenu(index){
           
         }
       }
-      console.log(index);
+      // console.log(index);
   }
 }
 
@@ -128,44 +85,78 @@ function openNav(){
 }
 
 function closeNav(){
-  console.log("closeNav()");
+  // console.log("closeNav()");
   menu.style.display="none";
   menuButton.style.display="flex";
   home.style.width = "95%";
   homeOverlay.style.opacity = 0;
 }
 
-function horizontalSliderSetChange(num){
-  console.log("ddd");
-  horizontalSliderIndexTemp = horizontalSliderIndex;
-  horizontalSliderIndex += num;
-  if(horizontalSliderIndex < 0){
-    horizontalSliderIndex = 0;
-  }
-  
-  if(horizontalSliderIndex > horizontalSlider.length){
-    horizontalSliderIndex = horizontalSlider.length-1;
-  }
-  horizontalSliderMakeChange();
- 
+function openProject(project){
+  // console.log(anchorLocation[project]);
+  canScroll = false;
+  hideAbleElements.hide();
+  $("#project-wrapper").fadeIn('slow');
+  $("#js-project-close").show();
+  $("#js-project-close").attr("pos", anchorLocation[project]);
+  $("body").css("overflow", "auto");
 }
 
-function horizontalSliderMakeChange(){
-    horizontalSlider[horizontalSliderIndexTemp].style.display = "none";
-    horizontalSlider[horizontalSliderIndex].style.display="block";
-    
-    if(horizontalSliderIndex != 0){
-      console.log('if');
-      canScroll = false;
-      menuButton = document.getElementById("menubutton");
-     // console.log(menuButton);
-     // console.log(menuButton.style.display);
-      menuButton.style.display="none";
-     // console.log(menuButton.style.display);
-    } else{
-      console.log('else');
-      canScroll = true;
-      menuButton.style.display="flex";
-      
-    }
+function closeProject(){
+
+  pos = $("#js-project-close").attr("pos");
+  console.log(pos);
+
+  $("body").css("overflow", "hidden");
+  $("#project-wrapper").hide();
+  $("#js-project-close").hide();
+  hideAbleElements.show();
+  canScroll = true;
+  window.scrollTo(0, pos);
+
 }
+
+
+$(document).on('mousewheel DOMMouseScroll', function (e) {
+  if(waiting || canScroll == false){
+    return;
+  }
+  e.preventDefault();//prevent the default mousewheel scrolling
+  var active = $('section.active');
+  var delta = e.originalEvent.detail < 0 || e.originalEvent.wheelDelta > 0 ? 1 : -1;
+  waiting = true;
+  if (delta < 0) {
+    anchorIndex += 1;
+    if(anchorIndex > (anchorPoints.length -1)){
+      anchorIndex = 0;
+    }
+    scrollTo({
+      top: anchorLocation[anchorIndex],
+      left: 0,
+      behavior: 'smooth'
+    });
+
+    // console.log(anchorIndex);
+    // console.log('scrolling down');
+  } else {
+    anchorIndex -= 1;
+    if(anchorIndex < 0){
+      anchorIndex = anchorPoints.length - 1;
+
+    }
+    scrollTo({
+      top: anchorLocation[anchorIndex],
+      left: 0,
+      behavior: 'smooth'
+    });
+    // console.log(anchorIndex);
+    // console.log('scrolling up');
+  }
+setTimeout(function(){
+  waiting = false;
+}, 1000);
+});
+
+$(window).on('beforeunload', function(){
+  $(window).scrollTop(0);
+});
